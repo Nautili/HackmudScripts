1803f0ce3b4aa8aae45fab72fafdd78f8eb8d14c
nautili <lear.vg@gmail.com>
2016-11-17 04:42:21 -0600

    Fixed bug in uncorrupt.js where indices were skipped.

--- scripts/uncorrupt.js
+++ scripts/uncorrupt.js
@@ -4,19 +4,22 @@ function (context, args) { //t:#s.name.call, args:""
     return res;
   }
 
+  // object to string and remove color codes
   var corruptChars = "¡¢£¤¥¦§¨©ª";
   var ret = JSON.stringify(args.t.call(args.args));
   var retArr = ret.replace(/`.([¡¢£¤¥¦§¨©ª])`/g, "$1").split("");
 
+  // find corrupt char locations
   var remainingIndices = [];
   for (var i = 0; i < retArr.length; i++) {
     if (corruptChars.indexOf(retArr[i]) > -1)
       remainingIndices.push(i);
   }
 
+  // repeatedly compare to new outputs to clear corrupt characters
   while (remainingIndices.length > 0) {
     var newStr = JSON.stringify(args.t.call(args.args)).replace(/`.([¡¢£¤¥¦§¨©ª])`/g, "$1");
-    for (var i = 0; i < remainingIndices.length; i++) {
+    for (var i = remainingIndices.length; i >= 0; --i) {
       if (corruptChars.indexOf(newStr[remainingIndices[i]]) < 0) {
         retArr[remainingIndices[i]] = newStr[remainingIndices[i]];
         remainingIndices.splice(i, 1);
